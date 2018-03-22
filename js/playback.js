49061392a41a47e48ad7d1e8b6d017bf14b79899
Neil McEachin <njmceach@mtu.edu>
2018-03-22 16:52:16 -0400

    added generic boxes for control buttons make progress width work with current time of video

--- js/playback.js
+++ js/playback.js
@@ -3,6 +3,7 @@ let playPause = document.getElementById("play-pause");
 let volume = document.getElementById("volume");
 let fullscreen = document.getElementById("fullscreen");
 let video = document.getElementById("vid");
+let progComplete = document.getElementById("progress-complete");
 
 playPause.addEventListener('click',()=>{
     if(playing === false){
@@ -15,3 +16,19 @@ playPause.addEventListener('click',()=>{
         playing = false;
     }
 });
+let progWidth = 0;
+let lastWidth = 0;
+window.setInterval(function(){   
+    if(playing === true){
+        progWidth = (video.currentTime/video.duration) * 100 + "%";
+        console.log(progWidth);
+        progComplete.style.width = progWidth;
+    }
+  }, 1000);
+
+video.volume=0;
+
+// let vid = document.getElementById("main-vid");
+// vid.style.width="100%";
+// vid.style.height="100%";
+// vid.style.background="cover";
